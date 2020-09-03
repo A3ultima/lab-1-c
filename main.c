3c9dc0b9b3af0d52175337f282f12c522ef91c9e
A3ultima <tps5616@psu.edu>
2020-09-03 13:57:47 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+//Author: Taoran Sheng
+//Collaborators: Aravind Hariprasad  azh5899@psu.edu
+//Collaborators: Kaitlyn Byrnes Krb5906@psu.edu
+//Collaborators: Kenleigh Leonard kml6565@psu.edu
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+int main(void) {
+  char *temp = readline("Enter temperature in celsius: ");
+  double c = atof(temp);
+  double f = c*1.8 +32;
+  printf("%f° in Celsius is equalvalent to %f° in Fahrenheit.", c, f);
+  return 0;
+}
